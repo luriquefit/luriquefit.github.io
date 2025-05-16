57f2988b015dbe8613bf9b91c2fd0cbe04238f11
pedrokamodaoficial <pedrokamoda04@gmail.com>
2025-05-16 18:30:12 -0300

    footer add

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-
 // Menu Lateral Interativo
-const heartMenu = document.getElementById('heartMenu');
-const sideMenu = document.getElementById('sideMenu');
-const overlay = document.getElementById('overlay');
+const heartMenu = document.getElementById("heartMenu");
+const sideMenu = document.getElementById("sideMenu");
+const overlay = document.getElementById("overlay");
 
-heartMenu.addEventListener('click', function() {
-    sideMenu.classList.toggle('active');
-    overlay.classList.toggle('active');
+heartMenu.addEventListener("click", function () {
+  sideMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
-overlay.addEventListener('click', function() {
-    sideMenu.classList.remove('active');
-    overlay.classList.remove('active');
+overlay.addEventListener("click", function () {
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 // Fechar menu ao clicar em um link
-document.querySelectorAll('.side-menu a').forEach(link => {
-    link.addEventListener('click', () => {
-        sideMenu.classList.remove('active');
-        overlay.classList.remove('active');
-    });
+document.querySelectorAll(".side-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    sideMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
 });
-
