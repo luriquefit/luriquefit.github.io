ea063bfc621561e5e5fb61920ea156bb78dbb6da
luriquefit <luriquefits@gmail.com>
2025-06-29 16:54:14 -0300

    deploy em react

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Wraper from "./components/Wraper";
+import bannerlogo from "./assets/bannerlogo.png";
+import bannermb from "./assets/bannermb.png";
+import bannerlu from "./assets/bannerlu.png";
 
 function App() {
+  // Exemplo simples para trocar imagem pelo tamanho da tela
+  const isDesktop = window.innerWidth >= 768;
+  const bannerBg = isDesktop ? bannerlu : bannermb;
+
   return (
     <div className="bg-black flex flex-col items-center">
       <Navbar />
       <div className="border-b-4 border-purple-800 w-full">
         <img
-          src="./src/assets/bannerlogo.png"
+          src={bannerlogo}
           alt="#logo"
           className="w-96 h-96 object-center object-cover mx-auto"
         />
       </div>
       {/* Banner background ocupa pelo menos 70% da tela */}
       <div
-        className="
-          w-full min-h-[70vh]
-          bg-[url('./src/assets/bannermb.png')]
-          md:bg-[url('./src/assets/bannerlu.png')]
-          bg-center bg-cover bg-fixed
-        "
+        className="w-full min-h-[70vh] bg-center bg-cover bg-fixed"
+        style={{
+          backgroundImage: `url(${bannerBg})`,
+        }}
       />
       <Wraper />
       <Hero />
